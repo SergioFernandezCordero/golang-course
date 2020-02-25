3c317cb91d96848b102a5374728e4293697fd6c0
El Autoestopista <elautoestopista@proyectopqmc.com>
2020-02-25 23:54:40 +0100

    Addede some exmaples from course

--- main.go
+++ main.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	// Primitives
+	var entero int
+	entero = 42
+	fmt.Println(entero)
+
+	var float float32 = 3.14
+	fmt.Println(float)
+
+	Nombre := "Manolo"
+	fmt.Println(Nombre)
+
+	var b bool = false
+	fmt.Println(b)
+
+	// pointers
+	var firstName *string = new(string)
+	*firstName = "Manolo"
+	fmt.Println(*firstName)
+
+	lastName := "Lopez"
+	fmt.Println(lastName)
+	ptr := &lastName
+	fmt.Println(ptr, *ptr)
+
+	// Constants
+	const pi = 3.1416
+	fmt.Println(pi + 3)
+	fmt.Println(pi + 1.2)
+
 }
